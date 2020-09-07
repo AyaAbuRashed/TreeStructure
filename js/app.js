a258226e482fec38bfdf0d710d71ac8fb290fd95
Hamza-Rashed <hamzarashed503@gmail.com>
2020-09-07 18:56:34 +0300

    old

--- js/app.js
+++ js/app.js
@@ -1,167 +1,87 @@
-// when the game began this window will tell the user about his preparing
-alert(' Are you ready ???')
-// this variable for print time
-var timer = document.getElementById('timer');
-// this variable for input and it value
-var input = document.getElementById('result');
-// this variable for the button when the user check on it
-var check = document.getElementById('button');
-// this is the first number
-var first = document.getElementById('first');
-// this is the second number
-var second = document.getElementById('second');
-// this is the operation 
-var operation = document.getElementById('operation');
-// this variable for the count of correct answer
-let trueAnswer = document.getElementById('true');
-// this variable for the count of wrong answer
-let falseAnswer = document.getElementById('false');
-// this variable for number of question
-let CountAllQuestions = document.getElementById('all');
-// this variable for find the operation from array and random
-var RandomOperationFind;
-// this variable for count the time
-let CountInterval;
-// this variable for excute the result from the sum proccess
-let ResultSum;
-// for excute the all result
-let ResultSubtraction;
-let Multiplication;
-let round;
-// for right and wrong answer
-let marksTrue = 0;
-let marksFales = 0;
-let allQuestions = 10;
+let changeDay = '';
 
-// this function for check on Enter key
-input.addEventListener('keydown', e => {
-    if (e.keyCode == 13) {
-        CheckResult();
-        Mounten();
-
-    }
-})
-
-// this function for check on the button
-check.onclick = () => {
-    CheckResult();
-    Mounten();
-
-}
-
-// this function content all the functions we need 
-let Mounten = () => {
-    var FirstArry = '1234567890';
-    var operationArry = '+-*/';
-    var ArryOperation = Array.from(operationArry)
-
-    // console.log(ArryOperation)
-
-    // Here i work about Conversion process all things to array 
-    var ArryFirst = Array.from(FirstArry)
-    // then i create the random for all operations
-    var RandomFirst = Math.floor(Math.random() * ArryFirst.length)
-    var RandomSecond = Math.floor(Math.random() * ArryFirst.length)
-    var RandomOperation = Math.floor(Math.random() * ArryOperation.length)
-
-    // this method for find the operation
-    ArryOperation.forEach((ele, index) => {
-        if (RandomOperation == index) {
-            RandomOperationFind = ele
-        }
-    })
-    // if statemnt for i can to know the correct answer
-    ResultAllOperations(RandomOperationFind, RandomSecond, RandomFirst)
-    // this function for return time again
-    clearInterval(CountInterval)
-    // just 3 seconds
-    CountDownTimer(3)
-    // for render the next question
-    StartGame(RandomFirst, RandomSecond, RandomOperationFind);
-    input.value = ''
-    EndGame();
-
-}
-
-var StartGame = (first, second, operation) => {
-    document.getElementById('first').innerHTML = first;
-    document.getElementById('second').innerHTML = second;
-    document.getElementById('operation').innerHTML = operation;
-}
-
-// this function for the time
-let CountDownTimer = (duration) => {
-    let seconds;
-    CountInterval = setInterval(function () {
-        // i create simple algorithm for print the second
-        seconds = parseInt(duration % 60);
-
-        timer.innerHTML = seconds;
-
-        if (--duration < 0) {
-            clearInterval(CountInterval)
-            CheckResult();
-            Mounten();
-        }
-
-    }, 1000)
-}
-// for know the correcr answer
-let ResultAllOperations = (RandomOperationFind, RandomSecond, RandomFirst) => {
-
-    if (RandomOperationFind == '+') {
-        ResultSum = RandomFirst + RandomSecond
-        console.log(ResultSum)
-    } else if (RandomOperationFind == '-') {
-        ResultSubtraction = RandomFirst - RandomSecond
-        console.log(ResultSubtraction)
-    } else if (RandomOperationFind == '*') {
-        Multiplication = RandomFirst * RandomSecond
-        console.log(Multiplication)
-    } else if (RandomOperationFind == '/') {
-        let RoundDivision = RandomFirst / RandomSecond
-        round = Math.round(RoundDivision)
-        console.log(round)
-    }
-
-}
-// for check the answer when user enter it on input if the answer ture or false
-let CheckResult = () => {
-    if (input.value == '') {
-        alert('False your answer is empty')
-        marksFales++
-        falseAnswer.innerHTML = marksFales;
-        allQuestions--
-        CountAllQuestions.innerHTML = allQuestions;
-    } else if (input.value == ResultSum || input.value == ResultSubtraction || input.value == Multiplication || input.value == round) {
-
-        alert('Great')
-        marksTrue++
-        trueAnswer.innerHTML = marksTrue;
-        allQuestions--
-        CountAllQuestions.innerHTML = allQuestions;
+let name = prompt("Enter your name please");
+if (name == '') {
+    console.log('your name is empty')
+} else {
+    console.log('welcome ' + name)
+    let age = prompt("Enter your age please");
+    if (Number(age) < 18) {
+        prompt("please " + name + " tell your father enter his phone number because your age under 18 ");
     } else {
-        alert('False')
-        marksFales++
-        falseAnswer.innerHTML = marksFales;
-        allQuestions--
-        CountAllQuestions.innerHTML = allQuestions;
-    }
-
-    if (allQuestions == 0) {
-        if (marksTrue == 10) {
-            alert('Congratulations You got full mark')
-            location.reload();
+        prompt("enter your phone number");
+        let food = prompt("what's kind of food do you like ?");
+        if (food) {
+            console.log("oooh that's perfect");
+            
         }
-        if (marksTrue >= 7) {
-            alert('good you got : ' + marksTrue)
-            location.reload();
-        }
-        if (marksTrue < 7) {
-            alert('Game Over your mark is bad : ' + marksTrue)
-            location.reload();
-        }
+        let cars = prompt("what's color of cars do you like ?");
+        if (cars == "red" || cars == "green" || cars == "blue" || cars == "yellow" || cars == "gray" || cars == "pink") {
+            alert('perfect color '+cars+' it is cool ')
+   
+          
     }
 }
+}
 
-Mounten();
+        let dayPrompt = prompt("What's today ??")
+        let day = new Date().getDay();
+        console.log(day)
+
+if(day == 0 && dayPrompt == 'Sunday') {
+    changeDay = 'Sunday'
+}else if(day == 1 && dayPrompt == 'Monday') {
+    changeDay = 'Monday'
+}else if(day == 2 && dayPrompt == "Tuesday") {
+    changeDay = 'Tuesday'
+}else if(day == 3 && dayPrompt == "Wednesday") {
+    changeDay = 'Wednesday'
+}else if(day == 4 && dayPrompt == "Thursday") {
+    changeDay = 'Thursday'
+}else if(day == 5 && dayPrompt == "Friday") {
+    changeDay = 'Friday'
+}else if(day == 6 && dayPrompt == "Saturday") {
+    changeDay = 'Saturday'
+}
+
+console.log(changeDay)
+        switch(dayPrompt) {
+            case changeDay == 'Sunday' &&  dayPrompt == 'Sunday':
+                alert('you are right it is Sunday')
+                break;
+                case changeDay == 'Monday' && dayPrompt == 'Monday':
+                alert('you are right it is Monday')
+                break;
+                case changeDay == 'Tuesday' && dayPrompt == 'Tuesday':
+                alert('you are right it is Tuesday')
+                break;
+                case changeDay == 'Wednesday' && dayPrompt == 'Wednesday':
+                alert('you are right it is Wednesday')
+                break;
+                case changeDay == 'Thursday' && dayPrompt == 'Thursday':
+                alert('you are right it is Thursday')
+                break;
+                case changeDay == 'Friday' && dayPrompt == 'Friday':
+                alert('you are right it is Friday')
+                break;
+                case changeDay == 'Saturday' && dayPrompt == 'Saturday' :
+                alert('you are right it is Saturday')
+                break;
+                default:
+                    if(day == 0) {
+                        alert('You are wrong today is : Sunday')
+                    }else if(day == 1) {
+                        alert('You are wrong today is : Monday')
+                    }else if(day == 2) {
+                        alert('You are wrong today is : Tuesday')
+                    }else if(day == 3) {
+                        alert('You are wrong today is : Wednesday')
+                    }else if(day == 4) {
+                        alert('You are wrong today is : Thursday')
+                    }else if(day == 5) {
+                        alert('You are wrong today is : Friday')
+                    }else if(day == 6) {
+                        alert('You are wrong today is : Saturday')
+                    }
+        }
+    
