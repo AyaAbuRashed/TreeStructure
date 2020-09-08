ad5ea6dc2ed2e88128fa1556390311d923b1fa73
Hamza-Rashed <hamzarashed503@gmail.com>
2020-09-09 00:05:08 +0300

    free

--- js/app.js
+++ js/app.js
@@ -1,102 +1,89 @@
-let changeDay = '';
-
-let name = prompt("do you know my name");
+// the first question about my name 
+let name = prompt("1 - do you know my name");
 if (name == '') {
     alert("the name is empty")
-} else if(name == 'hamza') {
+} else if (name == 'hamza') {
     alert('you are right perfect')
-}else{
+} else {
     alert('wrong')
 }
-    
-    let color = prompt("what is my favorite color");
-    if (color == '') {
-        alert('the color is empty pls choose one')
-        } else if(color == 'blue'){
-        alert("perfect that is right");
-        }else{
-            alert('wrong')
-        }
 
-let cars = prompt("what is kind of cars color do i like");
+// the second question about my favorite color
+let color = prompt("2 - what is my favorite color");
+if (color == '') {
+    alert('the color is empty pls choose one')
+} else if (color == 'blue') {
+    alert("perfect that is right");
+} else {
+    alert('wrong')
+}
+
+// the third question about My favorite car color
+let cars = prompt("3 - what is kind of cars color do i like");
 if (cars == '') {
     alert("the cars is empty")
-} else if(cars == 'green') {
+} else if (cars == 'green') {
     alert('you are right perfect')
-}else{
+} else {
     alert('wrong')
 }
-    
-    let food = prompt("what is my favorite food");
-    if (food == '') {
-        alert('the food is empty pls choose one')
-        } else if(food == 'beef'){
-        alert("perfect that is right");
-        }else{
-            alert('wrong')
-        }
 
+// I love lamb so this qustion about my best food 
+let food = prompt("4 - what is my favorite food");
+if (food == '') {
+    alert('the food is empty pls choose one')
+} else if (food == 'lamb') {
+    alert("perfect that is right");
+} else {
+    alert('wrong')
+}
 
-        let age = prompt("do you know my age");
+// about my age
+let age = prompt("5 - do you know my age");
 if (age == '') {
     console.log('the age is empty')
     alert("the age is empty")
-} else if(age == 24) {
+} else if (age == 24) {
     console.log('you are right perfect')
     alert('you are right perfect')
-}else{
+} else {
     alert('wrong')
 }
 
-let number = [33];
-let choose = prompt('choose a number')
-let secondChoose;
-let thirdChoose;
-let forthChoose;
-for(let i=0 ; i<number.length ; i++) {
-    if(choose == number[i]) {
+// this variable for while
+let i = 0;
+// this variable for check in the end if the answer false i will give him the right answer
+let check;
+while (i < 4) {
+    let choose = prompt('6 - choose a number')
+    if (choose == 33) {
         alert('perfect')
-    }else if(choose > number[i]) {
-        secondChoose = prompt('your answer very big pls again')
-    }else {
-        secondChoose = prompt('your answer very small pls again')
+        check = true;
+        break;
+    } else if (choose > 33) {
+        alert('your answer very big pls again')
+    } else {
+        alert('your answer very small pls again')
     }
-    if(secondChoose == number[i]) {
-        alert('perfect')
-    }else if(secondChoose > number[i]) {
-        thirdChoose = prompt('your answer very big pls again')
-    }else {
-        thirdChoose = prompt('your answer very small pls again')
-    }
-    if(thirdChoose == number[i]) {
-        alert('perfect')
-    }else if(thirdChoose > number[i]) {
-        forthChoose = prompt('your answer very big pls again')
-    }else {
-        forthChoose = prompt('your answer very small pls again')
-    }
-    if(forthChoose == number[i]) {
-        alert('perfect')
-    }else {
-        alert('sorry you failed the right answer is : '+number[i])
+    i++;
+}
+
+if (check != true) {
+    alert('sorry you failed the right answer is : 33')
+}
+
+// this the seven question about asken the user one of these numbers ... when loop finesh i will give the mark to user
+let arrayRandom = [12, 25, 31, 48, 120, 47]
+let rightAnswer = 0;
+for (let k = 0; k < 6; k++) {
+    let value = prompt('7 - Enter a number and take care you have a score')
+    let number = parseInt(value)
+    if (arrayRandom.includes(number)) {
+        alert('the next number')
+        rightAnswer++
+    } else {
+        alert('the next number')
     }
 }
 
-let arrayRandom = [12 , 35 , 75 , 9 , 23];
-
-// for() {
-    
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-    
+alert(`you got ${rightAnswer} marks`)
