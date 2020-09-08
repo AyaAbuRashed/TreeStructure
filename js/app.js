d6ae498ad5faf4c6b74385ad5e0eb9ae9101f4f0
Hamza-Rashed <62372964+Hamza-Rashed@users.noreply.github.com>
2020-09-08 14:54:14 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,87 +1,102 @@
 let changeDay = '';
 
-let name = prompt("Enter your name please");
+let name = prompt("do you know my name");
 if (name == '') {
-    console.log('your name is empty')
-} else {
-    console.log('welcome ' + name)
-    let age = prompt("Enter your age please");
-    if (Number(age) < 18) {
-        prompt("please " + name + " tell your father enter his phone number because your age under 18 ");
-    } else {
-        prompt("enter your phone number");
-        let food = prompt("what's kind of food do you like ?");
-        if (food) {
-            console.log("oooh that's perfect");
-            
+    alert("the name is empty")
+} else if(name == 'hamza') {
+    alert('you are right perfect')
+}else{
+    alert('wrong')
+}
+    
+    let color = prompt("what is my favorite color");
+    if (color == '') {
+        alert('the color is empty pls choose one')
+        } else if(color == 'blue'){
+        alert("perfect that is right");
+        }else{
+            alert('wrong')
         }
-        let cars = prompt("what's color of cars do you like ?");
-        if (cars == "red" || cars == "green" || cars == "blue" || cars == "yellow" || cars == "gray" || cars == "pink") {
-            alert('perfect color '+cars+' it is cool ')
-   
-          
+
+let cars = prompt("what is kind of cars color do i like");
+if (cars == '') {
+    alert("the cars is empty")
+} else if(cars == 'green') {
+    alert('you are right perfect')
+}else{
+    alert('wrong')
+}
+    
+    let food = prompt("what is my favorite food");
+    if (food == '') {
+        alert('the food is empty pls choose one')
+        } else if(food == 'beef'){
+        alert("perfect that is right");
+        }else{
+            alert('wrong')
+        }
+
+
+        let age = prompt("do you know my age");
+if (age == '') {
+    console.log('the age is empty')
+    alert("the age is empty")
+} else if(age == 24) {
+    console.log('you are right perfect')
+    alert('you are right perfect')
+}else{
+    alert('wrong')
+}
+
+let number = [33];
+let choose = prompt('choose a number')
+let secondChoose;
+let thirdChoose;
+let forthChoose;
+for(let i=0 ; i<number.length ; i++) {
+    if(choose == number[i]) {
+        alert('perfect')
+    }else if(choose > number[i]) {
+        secondChoose = prompt('your answer very big pls again')
+    }else {
+        secondChoose = prompt('your answer very small pls again')
+    }
+    if(secondChoose == number[i]) {
+        alert('perfect')
+    }else if(secondChoose > number[i]) {
+        thirdChoose = prompt('your answer very big pls again')
+    }else {
+        thirdChoose = prompt('your answer very small pls again')
+    }
+    if(thirdChoose == number[i]) {
+        alert('perfect')
+    }else if(thirdChoose > number[i]) {
+        forthChoose = prompt('your answer very big pls again')
+    }else {
+        forthChoose = prompt('your answer very small pls again')
+    }
+    if(forthChoose == number[i]) {
+        alert('perfect')
+    }else {
+        alert('sorry you failed the right answer is : '+number[i])
     }
 }
-}
 
-        let dayPrompt = prompt("What's today ??")
-        let day = new Date().getDay();
-        console.log(day)
+let arrayRandom = [12 , 35 , 75 , 9 , 23];
 
-if(day == 0 && dayPrompt == 'Sunday') {
-    changeDay = 'Sunday'
-}else if(day == 1 && dayPrompt == 'Monday') {
-    changeDay = 'Monday'
-}else if(day == 2 && dayPrompt == "Tuesday") {
-    changeDay = 'Tuesday'
-}else if(day == 3 && dayPrompt == "Wednesday") {
-    changeDay = 'Wednesday'
-}else if(day == 4 && dayPrompt == "Thursday") {
-    changeDay = 'Thursday'
-}else if(day == 5 && dayPrompt == "Friday") {
-    changeDay = 'Friday'
-}else if(day == 6 && dayPrompt == "Saturday") {
-    changeDay = 'Saturday'
-}
+// for() {
+    
+// }
 
-console.log(changeDay)
-        switch(dayPrompt) {
-            case changeDay == 'Sunday' &&  dayPrompt == 'Sunday':
-                alert('you are right it is Sunday')
-                break;
-                case changeDay == 'Monday' && dayPrompt == 'Monday':
-                alert('you are right it is Monday')
-                break;
-                case changeDay == 'Tuesday' && dayPrompt == 'Tuesday':
-                alert('you are right it is Tuesday')
-                break;
-                case changeDay == 'Wednesday' && dayPrompt == 'Wednesday':
-                alert('you are right it is Wednesday')
-                break;
-                case changeDay == 'Thursday' && dayPrompt == 'Thursday':
-                alert('you are right it is Thursday')
-                break;
-                case changeDay == 'Friday' && dayPrompt == 'Friday':
-                alert('you are right it is Friday')
-                break;
-                case changeDay == 'Saturday' && dayPrompt == 'Saturday' :
-                alert('you are right it is Saturday')
-                break;
-                default:
-                    if(day == 0) {
-                        alert('You are wrong today is : Sunday')
-                    }else if(day == 1) {
-                        alert('You are wrong today is : Monday')
-                    }else if(day == 2) {
-                        alert('You are wrong today is : Tuesday')
-                    }else if(day == 3) {
-                        alert('You are wrong today is : Wednesday')
-                    }else if(day == 4) {
-                        alert('You are wrong today is : Thursday')
-                    }else if(day == 5) {
-                        alert('You are wrong today is : Friday')
-                    }else if(day == 6) {
-                        alert('You are wrong today is : Saturday')
-                    }
-        }
+
+
+
+
+
+
+
+
+
+
+
     
